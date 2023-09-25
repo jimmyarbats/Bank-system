a9099295045790809c5867fb39ea83750dfb7254
jimmyarbats <jimmyaraujob@gmail.com>
2023-09-25 18:17:40 -0300

    req 07 done: implements Transfer()

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -120,7 +120,29 @@ public class Trybank
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        if (Bank[loggedUser, 3] < value)
+        {
+            throw new InvalidOperationException("Saldo insuficiente");
+        }
+
+        int destinationIndex = -1;
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+            {
+                destinationIndex = i;
+                break;
+            }
+        }
+        if (destinationIndex != -1)
+        {
+            Bank[loggedUser, 3] -= value;
+            Bank[destinationIndex, 3] += value;
+        }
     }
 
    
